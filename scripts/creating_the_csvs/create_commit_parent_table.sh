7e444326227bc587232dcecefd3dc80dd327f7ea
anishaislam8 <anishaislam8@gmail.com>
2023-12-09 22:59:33 -0700

    Update scripts

--- scripts/creating_the_csvs/create_commit_parent_table.sh
+++ scripts/creating_the_csvs/create_commit_parent_table.sh
@@ -1,5 +1,5 @@
 INPUT=project_name.txt
 while read p; do
-    python3 /parents_scripts/create_commit_parent_table_contents.py $p
+    python3 /parents_scripts/create_commit_parent_table.py $p
 done < $INPUT
 
